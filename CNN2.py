5807e3d013910eebce3935854e1d3602f272a3f9
shsjxzh <1927045093@qq.com>
2018-08-12 15:56:33 +0800

    fix bugs

--- CNN2.py
+++ CNN2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 
 # Hyper Parameters
-EPOCH = 1                       # the training times
+EPOCH = 6                       # the training times
 BATCH_SIZE = 64                 # not use all data to train
 LR = 0.001
 DOWNLOAD_MNIST = False          # if have already download, then turn it to 'False'
@@ -51,7 +51,8 @@ def train(model, device, train_loader, optimizer, criterion, epoch):
             print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / SHOW_STEP))
             running_loss = 0.0
 
-def test(model, device, test_loader, criterion):
+
+def evaluation(model, device, test_loader, criterion):
     model.eval()
     test_loss = 0
     correct = 0
@@ -59,52 +60,56 @@ def test(model, device, test_loader, criterion):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += criterion(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += criterion(output, target).sum().item()  # sum up batch loss
             pred = output.max(1, keepdim=True)[1]                           # get the index of max prob
-            correct = pred.eq(target.view_as(pred)).sum().item()
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    print('Test set: Average loss: %.4f, Accuracy: ')
-
-
+    print('Test set: Average loss: %.4f, Accuracy: %.4f %%' % (test_loss, 100. * correct / len(test_loader.dataset)))
 
 
 # transform: to tensor format and do batch normalization
 my_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
 
-train_data = torchvision.datasets.MNIST(
-    root='./mnist',             # the location to save
-    train=True,
-    download=DOWNLOAD_MNIST,
-    transform=my_transform
-)
 
-test_data = torchvision.datasets.MNIST(
-    root='./mnist',
-    train=False,
-    download=DOWNLOAD_MNIST,
-    transform=my_transform
-)
+def main():
+    train_data = torchvision.datasets.MNIST(
+        root='./mnist',             # the location to save
+        train=True,
+        download=DOWNLOAD_MNIST,
+        transform=my_transform
+    )
 
-# plt.imshow(train_data.train_data[0].numpy(), cmap='gray')
-# plt.title('%i' % train_data.train_labels[0])
-# plt.show()
+    test_data = torchvision.datasets.MNIST(
+        root='./mnist',
+        train=False,
+        download=DOWNLOAD_MNIST,
+        transform=my_transform
+    )
 
-print(test_data)
+    # plt.imshow(train_data.train_data[0].numpy(), cmap='gray')
+    # plt.title('%i' % train_data.train_labels[0])
+    # plt.show()
 
-train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
-test_loader = Data.DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+    test_loader = Data.DataLoader(dataset=test_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
 
-# total = test_loader.__len__()
-# print(total)
+    # total = test_loader.__len__()
+    # print(total)
 
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+    model = CNN().to(device)
 
-cnn = CNN()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LR)
+    loss_func = nn.CrossEntropyLoss()
 
-optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)
-loss_func = nn.CrossEntropyLoss()
+    for epoch in range(EPOCH):
+        train(model, device, train_loader, optimizer, loss_func, epoch)
+        evaluation(model, device, test_loader, loss_func)
 
-print('Finished Training')
+    print('Finished Training')
+
 
 if __name__ == '__main__':
     main()
