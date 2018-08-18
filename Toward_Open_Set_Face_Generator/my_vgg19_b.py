6f8be71435673fa92abd93cbe83949521d96afab
shsjxzh <1927045093@qq.com>
2018-08-18 16:09:05 +0800

    fix bug

--- Toward_Open_Set_Face_Generator/my_vgg19_b.py
+++ Toward_Open_Set_Face_Generator/my_vgg19_b.py
@@ -57,7 +57,7 @@ def my_vgg19_b(pretrained=False, **kwargs):
         kwargs['init'] = False
     model = my_vgg(make_layers(cfg), **kwargs)
     if pretrained:
-        model.load_state_dict(torch.load('c_params.pkl'), map_location=lambda storage, loc: storage)
+        model.load_state_dict(torch.load('c_params.pkl',  map_location=lambda storage, loc: storage))
     return model
 
 # print(my_vgg19_b())
