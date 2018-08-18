7b5f80d6f053979435216078110346b057822ed5
shsjxzh <1927045093@qq.com>
2018-08-18 21:06:46 +0800

    change the model

--- Toward_Open_Set_Face_Generator/my_vgg19_b.py
+++ Toward_Open_Set_Face_Generator/my_vgg19_b.py
@@ -3,9 +3,12 @@ import torch.nn as nn
 
 class my_vgg(nn.Module):
     # pic_size need to be caculated carefully
-    def __init__(self, features, num_classes=1000, pic_size=512 * 4 * 4, hidden_node=4096, init=True):
+    def __init__(self, num_classes=1000, pic_size=512 * 4 * 4, hidden_node=4096, init=True):
         super(my_vgg, self).__init__()
-        self.features = features
+        
+        cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+        
+        self.features = self._make_layers(cfg)
         self.classifier = nn.Sequential(
             nn.Linear(pic_size, hidden_node),
             nn.ReLU(True),
@@ -36,28 +39,26 @@ class my_vgg(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
+                
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for v in cfg:
+            if v == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                in_channels = v
 
-def make_layers(cfg):
-    layers = []
-    in_channels = 3
-    for v in cfg:
-        if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-        else:
-            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
-            in_channels = v
-
-    return nn.Sequential(*layers)
-
-cfg = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+        return nn.Sequential(*layers)
 
 def my_vgg19_b(pretrained=False, **kwargs):
     if pretrained:
         kwargs['init'] = False
-    model = my_vgg(make_layers(cfg), **kwargs)
+    model = my_vgg(**kwargs)
     if pretrained:
         model.load_state_dict(torch.load('c_params.pkl',  map_location=lambda storage, loc: storage))
     return model
 
-# print(my_vgg19_b())
+print(my_vgg19_b())
