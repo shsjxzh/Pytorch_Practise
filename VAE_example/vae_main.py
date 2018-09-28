6601166af8d98c4f68dbf9b60a542d38530e2ff1
shsjxzh <1927045093@qq.com>
2018-09-28 19:00:07 +0800

    try use vae

--- VAE_example/vae_main.py
+++ VAE_example/vae_main.py
@@ -1,9 +1,12 @@
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data as Data 
 import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
+from torchvision.utils import save_image
+import os
 
 # Hyper Parameters
 EPOCH = 200                     # the training times
@@ -11,8 +14,8 @@ BATCH_SIZE = 64                 # not use all data to train
 LR = 0.001
 DOWNLOAD_MNIST = False          # if have already download, then turn it to 'False'
 SHOW_STEP = 100                 # show the result after how many steps
-use_gpu = False
-DeviceID = [0]
+use_gpu = True
+DeviceID = [2]
 
 class VAE(nn.Module):
     def __init__(self):
@@ -27,9 +30,9 @@ class VAE(nn.Module):
         self.log_var = nn.Sequential(nn.Linear(128 * 14 * 14, 1024))
         self.my_mean = nn.Sequential(nn.Linear(128 * 14 * 14, 1024))
         self.reconstract = nn.Sequential(
-            nn.Linear(1024, 1024)
-            nn.ReLU()
-            nn.Linear(1024, 28 * 28)
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 28 * 28),
             nn.Tanh()
         )
     
@@ -43,7 +46,7 @@ class VAE(nn.Module):
         
         eps = torch.normal(torch.zeros_like(std), 1)
         if use_gpu:
-            device = torch.device("cuda:" + str(DeviceID[0]))
+            device = torch.device("cuda:" + str(my_mean.get_device()))
         else:
             device = torch.device("cpu")
 
@@ -57,24 +60,23 @@ class VAE(nn.Module):
     def forward(self, x):
         my_mean, log_var = self.encode(x)
         z = self.reparametrize(my_mean, log_var)
-        return self.decode(z), my_mean， log_var
+        return self.decode(z), my_mean, log_var
+
+def to_img(x):
+    x = x.clamp(0, 1)
+    x = x.view(x.size(0), 1, 28, 28)
+    return x
 
 # transform: to tensor format and do batch normalization
-my_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
-
-
-def loss_func(recon_x, x, mu, logvar):
-    """
-    recon_x: generating images
-    x: origin images
-    mu: latent mean
-    logvar: latent log variance
-    """
-
-    BCE = nn.MSELoss(recon_x, x, size_average=False)   # This is the reconstruction loss
-
+my_transform = transforms.Compose([
+    transforms.ToTensor(), 
+    # transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
 
 def main():
+    if not os.path.exists('./vae_img'):
+        os.mkdir('./vae_img')
+
     train_data = torchvision.datasets.MNIST(
         root='./mnist',             # the location to save
         train=True,
@@ -98,7 +100,8 @@ def main():
         device = torch.device("cpu")
 
     model = VAE()
-    model = nn.DataParallel(model, device_ids=DeviceID).to(device)
+    if use_gpu:
+        model = nn.DataParallel(model, device_ids=DeviceID).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     
     # train
@@ -106,13 +109,15 @@ def main():
     running_loss = 0.0
     for epoch in range(EPOCH):
         for batch_idx, (image, identity) in enumerate(train_loader):
+            image, identity = image.to(device), identity.to(device)
             recon_batch, my_mean, log_var = model(image)
 
             # reconstruction loss
-            L_R = nn.MSELoss(recon_batch, image.view(image.size(0), -1))
+            rcstrc_loss = nn.MSELoss(size_average=False)
+            L_R = rcstrc_loss(recon_batch, image.view(image.size(0), -1))
 
             # KL Divergence loss
-            L_KLD = 0.5 * (my_mean.pow(2) + log_var.exp() - log_var - 1)
+            L_KLD = 0.5 * (my_mean.pow(2) + log_var.exp() - log_var - 1).sum()
 
             loss = L_R + L_KLD
 
@@ -125,12 +130,12 @@ def main():
                 print('[%d, %5d] loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / SHOW_STEP))
                 running_loss = 0.0
 
+        if epoch % 10 == 0:
+            save = to_img(recon_batch.cpu().data)
+            save_image(save, './vae_img/image_{}.png'.format(epoch + 1))
+            torch.save(model.state_dict(), './vae_params.pkl')
 
-        if epoch % 10 == 9:
-        save = to_img(recon_batch.cpu().data)
-        save_image(save, './vae_img/image_{}.png'.format(epoch + 1))
-        torch.save(model.state_dict(), './vae_params.pkl')
-
-        
+if __name__ == '__main__':
+    main()
 
 
